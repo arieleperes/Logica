06fde83d399c6079f51f30a6ac5c571d29e7a858
Ariele peres <ariele.peres@outlook.com>
2024-03-25 21:49:27 -0300

    add

--- pythonProject/aula06/exercicio02.py
+++ pythonProject/aula06/exercicio02.py
@@ -3,10 +3,14 @@ valor= float(input('digite o valor da compra'))
 
 if valor< 1000:
     desconto= valor * 0.1
-    print('o desconto foi de ', desconto)
+    desconto_total = valor - desconto
+    print('o desconto foi de ', desconto_total)
 elif valor< 5000:
     desconto=  valor * 0.2
-    print('o desconto foi de', desconto)
+    desconto_total = valor - desconto
+    print('o desconto foi de', desconto_total)
 else:
     desconto= valor * 0.3
-    print('o desconto foi de', desconto)
+    desconto_total = valor - desconto
+    print('o desconto foi de', desconto_total)
+
