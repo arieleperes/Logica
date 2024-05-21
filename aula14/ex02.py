54c52de85218ae33c4498a378904c8d189ea5479
Ariele peres <ariele.peres@outlook.com>
2024-05-20 21:59:16 -0300

    add

--- aula14/ex02.py
+++ aula14/ex02.py
@@ -1,1 +1,13 @@
-def primo
+def eprimo(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, int(n/2)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def inteiro(n):
+    if n > 0:
+        
